0876c400c796eeef3ce694a3aa311f47503b047a
Malaikat7 <145450568+Malaikat7@users.noreply.github.com>
2024-05-31 16:05:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const image = document.querySelector('#pompom-image');
+const neutralpompom = document.querySelector('#pompom-image');
 const hereforyouButton = document.querySelector('#btn1');
-const petherButton = document.querySelector('#btn2');
+const lovelyButton = document.querySelector('#btn2');
 const yourecuteButton = document.querySelector('#btn3');
+const pompomImage = document.querySelector('#pompom-image');
 
-// Array images
-const images = [
+// Array images, hulp van chatGPT
+const pompomhappinessgrowing = [
     'images/pompom1.png', // 10%
     'images/pompom2.png', // 25%
     'images/pompom3.png', // 50%
@@ -12,45 +13,29 @@ const images = [
     'images/pompom5.png'  // 100%
 ];
 
-// Array specifieke levels
-const happinessLevels = [10, 25, 50, 75, 100];
+// https://youtu.be/aTiOiMuSznE?si=9e8gw5b65YNpukYa, audio code van chatGPT
+const clickSound = new Audio('audio/twinkleaudio.mpeg');
 
 // begin happiness level
 let happiness = 10;
-let happinessindex = 0
+let happinessindex = 0;
 
-// Change image per happiness level
-function changeImage() {
-    const pompomImage = document.querySelector('#pompom-image');
-    if (happiness >= 100) {
-        pompomImage.src = images[4];
-    } else if (happiness >= 75) {
-        pompomImage.src = images[3];
-    } else if (happiness >= 50) {
-        pompomImage.src = images[2];
-    } else if (happiness >= 25) {
-        pompomImage.src = images[1];
-    } else {
-        pompomImage.src = images[0];
-    }
+//met hulp van student
+function growinghappiness(){
+
+if (happinessindex < pompomhappinessgrowing.length){
+
+    neutralpompom.src = pompomhappinessgrowing[happinessindex];
+// audio code van chatGPT
+    clickSound.play();
+
+    happinessindex = happinessindex + 1;
+}   else if (happinessindex == pompomhappinessgrowing.length){
+    tekst.textContent = "pompom is happy"}
+
 }
 
-function changeImage() {
-    image.src = 'images/pompom2.png';
-}
+hereforyouButton.addEventListener('click', growinghappiness);
+lovelyButton.addEventListener('click', growinghappiness);
+yourecuteButton.addEventListener('click', growinghappiness);
 
-function changeImage() {
-    image.src = 'images/pompom3.png';
-}
-
-function changeImage() {
-    image.src = 'images/pompom4.png';
-}
-
-hereforyouButton.addEventListener('click', changeImage);
-petherButton.addEventListener('click', changeImage);
-yourecuteButton.addEventListener('click', changeImage);
-
-document.querySelector('#btn1').addEventListener('click', ButtonClick);
-document.querySelector('#btn2').addEventListener('click', ButtonClick);
-document.querySelector('#btn3').addEventListener('click', ButtonClick);
