74bef80e301a2d1609579725d86f8d6afeb7d000
Malaikat7 <145450568+Malaikat7@users.noreply.github.com>
2024-06-01 16:47:27 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,11 @@ const pompomImage = document.querySelector('#pompom-image');
 
 // Array images, hulp van chatGPT
 const pompomhappinessgrowing = [
-    'images/pompom1.png', // 10%
-    'images/pompom2.png', // 25%
-    'images/pompom3.png', // 50%
-    'images/pompom4.png', // 75%
-    'images/pompom5.png'  // 100%
+    'images/pompom1.jpg', // 10%
+    'images/pompom2.jpg', // 25%
+    'images/pompom3.jpg', // 50%
+    'images/pompom4.jpg', // 75%
+    'images/pompom5.jpg'  // 100%
 ];
 
 // https://youtu.be/aTiOiMuSznE?si=9e8gw5b65YNpukYa, audio code van chatGPT
@@ -22,17 +22,14 @@ let happinessindex = 0;
 
 //met hulp van student
 function growinghappiness(){
-
-if (happinessindex < pompomhappinessgrowing.length){
-
-    neutralpompom.src = pompomhappinessgrowing[happinessindex];
+    if (happinessindex < pompomhappinessgrowing.length){
+        neutralpompom.src = pompomhappinessgrowing[happinessindex];
 // audio code van chatGPT
-    clickSound.play();
-
-    happinessindex = happinessindex + 1;
-}   else if (happinessindex == pompomhappinessgrowing.length){
-    tekst.textContent = "pompom is happy"}
-
+        clickSound.play();
+        happinessindex = happinessindex + 1;
+    } else {
+        alert("PomPom is fully happy, thank you!"); // hulp van chatGPT
+    }
 }
 
 hereforyouButton.addEventListener('click', growinghappiness);
